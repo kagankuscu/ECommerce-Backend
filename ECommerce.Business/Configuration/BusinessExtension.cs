3bd6e3b0084f08001bf1b083110c66b87fdf7d3c
kagankuscu <kuscukagan@gmail.com>
2024-06-09 08:58:33 +0300

    Initial Business

---
+++ ECommerce.Business/Configuration/BusinessExtension.cs
@@ -0,0 +1,12 @@
+﻿using ECommerce.Repository.Abstract;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ECommerce.Business.Configuration;
+
+public static class BusinessExtension
+{
+    public static void AddRepository(this IServiceCollection service)
+    {
+        service.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+    }
+}
