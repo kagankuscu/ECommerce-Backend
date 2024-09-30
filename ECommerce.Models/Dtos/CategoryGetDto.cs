75ba352bcbce758dbbb6ff6aeaa4b6b5ec14302c
kagankuscu <kuscukagan@gmail.com>
2024-09-30 11:40:07 +0300

    CategoryGetDto Category field added

--- ECommerce.Models/Dtos/CategoryGetDto.cs
+++ ECommerce.Models/Dtos/CategoryGetDto.cs
@@ -3,6 +3,20 @@ namespace ECommerce.Models.Dtos
     public class CategoryGetDto
     {
         public int Id { get; set; }
-        public string CategoryName { get; set; } = string.Empty;
+        public string CategoryName { private get; set; } = string.Empty;
+        public string Category
+        {
+            get
+            {
+                var names = CategoryName.Split(' ');
+                var newNames = new List<string>();
+                foreach(var name in names)
+                {
+                    var n = name.Substring(0, 1).ToUpper() + name.Substring(1).ToLower();
+                    newNames.Add(n);
+                }
+                return string.Join(" ", newNames);
+            }
+        }
     }
 }
